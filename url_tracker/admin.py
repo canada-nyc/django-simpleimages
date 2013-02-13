b6ab1717fe6db84626854edb035bba53dc1ad169
Jannis Leidel <jannis@leidel.info>
2013-02-13 15:49:14 +0100

    Improved the admin interface.

--- url_tracker/admin.py
+++ url_tracker/admin.py
@@ -2,4 +2,9 @@ from django.contrib import admin
 
 from url_tracker.models import URLChangeRecord
 
-admin.site.register(URLChangeRecord)
+
+class URLChangeRecordAdmin(admin.ModelAdmin):
+    list_display = ('__unicode__', 'deleted', 'date_changed')
+    list_filter = ('deleted', 'date_changed')
+
+admin.site.register(URLChangeRecord, URLChangeRecordAdmin)
