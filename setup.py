1e8ac5b83dbd3034ad35ae8bfb1de1483eb04551
Saul Shanabrook <s.shanabrook@gmail.com>
2013-03-20 09:42:55 -0400

    New Version and fixed testing

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='django-simpleimages',
-    version='0.1.0',
+    version='0.1.1',
     author='Saul Shanabrook',
     author_email='s.shanabrook@gmail.com',
     packages=['simpleimages', 'simpleimages.test'],
