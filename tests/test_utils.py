fd3870ac2edeeecb80dafabf0923b50ac9a24339
Saul Shanabrook <s.shanabrook@gmail.com>
2013-08-21 03:11:27 -0400

    Change utile tests to py.test format

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,132 +1,47 @@
-from django.test import TestCase
-from django.core.files.base import ContentFile
-from django.test.utils import override_settings
+import pytest
 
-from simpleimages.utils import perform_transformation
-from . import utils
+from django.core.files.images import get_image_dimensions
+
 from .models import TestModel
 
 
-class PerformTransformationTest(utils.RemoveStorage, TestCase):
-    def setUp(self):
-        self.default_dimensions = (200, 200)
-        self.image_name = 'image.jpg'
-        self.non_image_file = ContentFile(u'some content')
+class TestTransformField:
+    @pytest.fixture()
+    def instance(self, db):
+        return TestModel.objects.create()
 
-        self.model = TestModel.objects.create()
-        self.model2 = TestModel.objects.create()
-        self.model.image.save(
-            self.image_name,
-            utils.django_image(
-                *self.default_dimensions,
-                name=self.image_name
-            )
-        )
-        self.model2.image.save(
-            '2' + self.image_name,
-            utils.django_image(
-                *self.default_dimensions,
-                name='2' + self.image_name
-            )
-        )
+    @pytest.fixture()
+    def instance_with_source(self, image, instance):
+        instance.image.save(image.name, image.django_file)
+        return instance
 
-    def test_initial_blank(self):
-        self.assertFalse(self.model.thumbnail)
-        self.assertFalse(self.model2.thumbnail)
+    @pytest.fixture()
+    def instance_with_source_and_thumb(self, image, instance_with_source):
+        source_dimensions = get_image_dimensions(instance_with_source.image)
+        image.dimensions = [dimension + 1 for dimension in source_dimensions]
+        instance_with_source.thumbnail.save(image.name, image.django_file)
+        return instance_with_source
 
-    def test_transform_creates_another_image(self):
-        perform_transformation([self.model])
+    def test_blank_transform_wont_save(self, instance_with_source):
+        instance_with_source._transform(lambda file: None)
 
-        self.non_cached_model = TestModel.objects.get(pk=self.model.pk)
-        self.non_cached_model2 = TestModel.objects.get(pk=self.model2.pk)
+        assert not instance_with_source.thumbnail
 
-        self.assertTrue(self.model.thumbnail)
-        self.assertFalse(self.model2.thumbnail)
-        self.assertTrue(self.non_cached_model.thumbnail)
-        self.assertFalse(self.non_cached_model2.thumbnail)
+    def test_will_save(self, instance_with_source):
+        instance_with_source._transform()
 
-    def test_transformed_image_is_different(self):
-        perform_transformation([self.model])
+        assert instance_with_source.thumbnail
 
-        self.non_cached_model = TestModel.objects.get(pk=self.model.pk)
+    def test_no_overwrite(self, settings, instance_with_source_and_thumb):
+        settings.SIMPLEIMAGES_OVERWRITE = False
 
-        self.assertEqual(self.model.thumbnail.width, 10)
-        self.assertEqual(self.non_cached_model.thumbnail.width, 10)
+        instance_with_source_and_thumb._transform()
 
-    def test_transform_two_instances(self):
-        perform_transformation([self.model, self.model2])
+        assert not instance_with_source_and_thumb.image_dimensions == instance_with_source_and_thumb.thumbnail_dimensions
 
-        self.non_cached_model = TestModel.objects.get(pk=self.model.pk)
-        self.non_cached_model2 = TestModel.objects.get(pk=self.model2.pk)
+    def test_yes_overwrite(self, settings, instance_with_source_and_thumb):
+        settings.SIMPLEIMAGES_OVERWRITE = True
 
-        self.assertTrue(self.model.thumbnail)
-        self.assertTrue(self.model2.thumbnail)
-        self.assertTrue(self.non_cached_model.thumbnail)
-        self.assertTrue(self.non_cached_model2.thumbnail)
+        instance_with_source_and_thumb._transform()
 
-    def test_transform_queryset(self):
-        perform_transformation(TestModel.objects.all())
-
-        self.non_cached_model = TestModel.objects.get(pk=self.model.pk)
-        self.non_cached_model2 = TestModel.objects.get(pk=self.model2.pk)
-
-        self.assertTrue(self.non_cached_model.thumbnail)
-        self.assertTrue(self.non_cached_model2.thumbnail)
-
-    def test_transform_wrong_field(self):
-        perform_transformation([self.model], field_names=['sdf'])
-
-        self.non_cached_model = TestModel.objects.get(pk=self.model.pk)
-
-        self.assertFalse(self.model.thumbnail)
-        self.assertFalse(self.non_cached_model.thumbnail)
-
-    def test_transform_right_field(self):
-        perform_transformation([self.model], field_names=['image'])
-
-        self.non_cached_model = TestModel.objects.get(pk=self.model.pk)
-
-        self.assertTrue(self.model.thumbnail)
-        self.assertTrue(self.non_cached_model.thumbnail)
-
-    def test_transform_error_doesnt_save_file(self):
-        self.model.image.save(
-            self.image_name,
-            self.non_image_file
-        )
-        perform_transformation([self.model], field_names=['image'])
-        self.non_cached_model = TestModel.objects.get(pk=self.model.pk)
-
-        self.assertTrue(self.model.image)
-        self.assertTrue(self.non_cached_model.image)
-
-        self.assertFalse(self.model.thumbnail)
-        self.assertFalse(self.non_cached_model.thumbnail)
-
-    def test_save_path(self):
-        perform_transformation([self.model])
-        self.assertNotIn('originals', self.model.thumbnail.path)
-
-    @override_settings(SIMPLEIMAGES_OVERWRITE=False)
-    def test_wont_overwrite_on_settings(self):
-        self.model.image.save(
-            self.image_name,
-            utils.django_image(
-                *(2, 2),
-                name=self.image_name
-            )
-        )
-        perform_transformation([self.model])
-
-        self.model.image.save(
-            self.image_name,
-            utils.django_image(
-                *(10, 10),
-                name=self.image_name
-            )
-        )
-        perform_transformation([self.model])
-
-        self.non_cached_model = TestModel.objects.get(pk=self.model.pk)
-        self.assertEqual(self.model.thumbnail.width, 2)
-        self.assertEqual(self.non_cached_model.thumbnail.width, 2)
+        assert instance_with_source_and_thumb.image_dimensions == instance_with_source_and_thumb.thumbnail_dimensions
