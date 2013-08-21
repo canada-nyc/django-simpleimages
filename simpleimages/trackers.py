a8216c0cff864cbce17a71c56ed2bc7ba7ca9afc
Saul Shanabrook <s.shanabrook@gmail.com>
2013-08-21 01:25:29 -0400

    Added docstrings to tracking

--- simpleimages/trackers.py
+++ simpleimages/trackers.py
@@ -1,13 +1,23 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-from . import utils
+import simpleimages.utils
 
 
 def track_model(model):
+    '''
+    Perform designated transformations on model, when it saves.
+
+    Calls :py:func:`~simpleimages.utils.perform_transformation`
+    on every model saves using
+    :py:data:`django.db.models.signals.post_save`.
+
+    It uses the ``update_fields`` kwarg to tell what fields it should
+    transform.
+    '''
     @receiver(post_save, sender=model, weak=False, dispatch_uid='simpleimages')
     def transform_signal(sender, **kwargs):
-        utils.perform_transformation(
+        simpleimages.utils.perform_transformation(
             kwargs['instance'],
             kwargs['update_fields']
         )
