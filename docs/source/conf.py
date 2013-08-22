b1037931d6a63449088eaf1d15226a69497e54fd
Saul Shanabrook <s.shanabrook@gmail.com>
2013-08-22 13:37:15 -0400

    Add packages to path for RTFD

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,12 +13,12 @@
 
 import sys, os
 
-import sphinx_bootstrap_theme
-
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('../simpleimages'))
+sys.path.insert(0, os.path.abspath('../tests'))
 
 # -- Custom Configuration ------------------------------------------------------
 
