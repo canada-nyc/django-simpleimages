fface01ae4a41151e8874e13d8566981d01901b7
Saul Shanabrook <s.shanabrook@gmail.com>
2013-08-22 21:56:18 -0400

    Added proper trove classifiers [ci skip]

--- setup.py
+++ setup.py
@@ -12,8 +12,25 @@ setup(
     description='Opinionated Django image transforms on models',
     long_description=open('README.rst').read(),
     install_requires=[
-        "Django>=1.5,<1.6",
+        "Django>=1.5,<=1.6",
         "six"
     ],
-    zip_safe=False  # so that django finds management commands
+    zip_safe=False,  # so that django finds management commands,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Framework :: Django',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: Unix',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Topic :: Software Development :: Libraries',
+    ],
 )
