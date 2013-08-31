3db4d85164a2a2c41503b568505077cf2af86e90
Saul Shanabrook <s.shanabrook@gmail.com>
2013-08-31 18:35:36 -0400

    Add management file to packages so django can find it

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     version='1.0.0',
     author='Saul Shanabrook',
     author_email='s.shanabrook@gmail.com',
-    packages=['simpleimages', ],
+    packages=['simpleimages', 'simpleimages.management.commands'],
     url='https://www.github.com/saulshanabrook/django-simpleimages',
     license=open('LICENSE.txt').read(),
     description='Opinionated Django image transforms on models',
