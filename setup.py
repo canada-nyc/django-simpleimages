1a62115e3146e8a74eb1bb407f811eb92c6a943e
Saul <s.shanabrook@gmail.com>
2014-01-14 22:59:19 -0500

    Bumped version.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='django-simpleimages',
-    version='1.0.5',
+    version='1.1.0',
     author='Saul Shanabrook',
     author_email='s.shanabrook@gmail.com',
     packages=[
